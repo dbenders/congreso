e80bfcc40ff2ae8c8e7cf3720e9064a3a6d29f9d
Diego Bendersky <diegobendersky@gmail.com>
2013-05-29 20:30:50 -0300

    blah

--- congreso/app/controllers/admin/sessions_controller.rb
+++ congreso/app/controllers/admin/sessions_controller.rb
@@ -2,29 +2,14 @@ include ApplicationHelper
 
 class Admin::SessionsController < ApplicationController
 
-  # GET /sessions
-  # GET /sessions.json
+  # GET /admin/sessions
+  # GET /admin/sessions.json
   def index
     @sessions = Session.all
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @sessions }
-    end
-  end
-
-  # GET /sessions/1
-  # GET /sessions/1.json
-  def show    
-    @tbk = TextBookmark.find(params[:tbk]) if params[:tbk]
-    if params[:period] and params[:meeting]
-      @session = Session.find_by_period_and_meeting(params[:period],params[:meeting])
-    else
-      @session = Session.find(params[:id])
-    end
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @session }
     end
   end
 
@@ -86,8 +71,8 @@ class Admin::SessionsController < ApplicationController
     end
   end
 
-  # GET /sessions/new
-  # GET /sessions/new.json
+  # GET /admin/sessions/new
+  # GET /admin/sessions/new.json
   def new
     @session = Session.new
 
@@ -97,13 +82,13 @@ class Admin::SessionsController < ApplicationController
     end
   end
 
-  # GET /sessions/1/edit
+  # GET /admin/sessions/1/edit
   def edit
     @session = Session.find(params[:id])
   end
 
-  # POST /sessions
-  # POST /sessions.json
+  # POST /admin/sessions
+  # POST /admin/sessions.json
   def create
     @session = Session.new(params[:session].except(:chamber_id))
     @chamber = Chamber.find(params[:session][:chamber_id])
@@ -119,15 +104,15 @@ class Admin::SessionsController < ApplicationController
     end
   end
 
-  # PUT /sessions/1
-  # PUT /sessions/1.json
+  # PUT /admin/sessions/1
+  # PUT /admin/sessions/1.json
   def update
     @session = Session.find(params[:id])
     @chamber = Chamber.find(params[:session][:chamber_id])
     respond_to do |format|
       if @session.chamber = @chamber and @session.you_tube_video_ids = params[:session][:you_tube_video_ids] and
                 @session.update_attributes(params[:session].except(:chamber_id).except(:you_tube_video_ids))
-        format.html { redirect_to @session, notice: 'Session was successfully updated.' }
+        format.html { redirect_to admin_sessions_path, notice: 'Session was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -136,8 +121,8 @@ class Admin::SessionsController < ApplicationController
     end
   end
 
-  # DELETE /sessions/1
-  # DELETE /sessions/1.json
+  # DELETE /admin/sessions/1
+  # DELETE /admin/sessions/1.json
   def destroy
     @session = Session.find(params[:id])
     @session.destroy
---
+++ congreso/test/integration/session_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SessionTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
